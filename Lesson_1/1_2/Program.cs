e72cd1c8e841e1a958209a2bed5445072532164e
Nazi Mikhelashvili <nazomikhelashvili@gmail.com>
2024-01-07 16:49:24 +0300

    Lesson 1 Task 2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача: показать все числа от -N до N
+
+Console.Write ("Enter Number ");
+int a = int.Parse (Console.ReadLine ()!);
+int count = a * -1;
+
+while (count <= a)
+{
+Console.Write(count+" ");
+count++;
+}
