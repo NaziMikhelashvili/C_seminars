0a2b89367c5c35165a5c5cc1ec6be8b3f75cb7f1
Nazi Mikhelashvili <nazomikhelashvili@gmail.com>
2024-01-07 16:27:27 +0300

    Lesson 1 Task 1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача: является ли первое число квадратом второго?
+
+Console.Write ("Enter Number1 ");
+int b1 = int.Parse (Console.ReadLine ()!);
+Console.Write ("Enter Number2 ");
+int b2 = int.Parse (Console.ReadLine ()!);
+
+if (b2 * b2 == b1)
+Console.WriteLine("Yes");
+else
+Console.WriteLine("No");
